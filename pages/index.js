ba87a51967bdee17f596f17678903d99a66f0450
fullstackaubrey <aubreywebdev@proton.me>
2023-01-31 11:29:43 +1300

    added Link

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { name, siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home({  }) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm {name}. I'm a Yoga Instructor and a Software Engineer.</p>
+        <p>Hello, I'm {name}! I'm a Software Engineer and Yoga Instructor.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+        <p>
+          <Link href="/posts/first-post">Take me to a new page!</ Link>
         </p>
       </section>
     </Layout>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -6,14 +6,17 @@ function FirstPost() {
   return (
     <Layout>
       <Head>
-        <title>First Post</title>
+        <title>Blog: First Post</title>
       </Head>
-      
-      <h1>First Post</h1>
-
-      <h2>
+      <h2>First Post</h2>
+      <section>
+        <p>Lorem ipsum dolor sit amet. Vel minus distinctio vel voluptate voluptas non veniam illo qui suscipit porro. Et voluptas eaque est asperiores culpa nam debitis labore ea enim dolores et autem modi qui suscipit veritatis? Sit quasi galisum qui placeat minus eum consequatur quis.</p>
+        <p>Et molestiae vitae At nihil reprehenderit qui deleniti officiis? Quo maiores blanditiis non laudantium nulla eum excepturi repudiandae et eaque doloremque sed dolorum voluptatem.</p>
+        <p>Ut laboriosam blanditiis eum ipsum illum vel eveniet accusantium eos doloremque sunt ut doloremque nesciunt. Ab aliquid expedita rem beatae sint et dolor sapiente qui nostrum vitae sit sint dolorem nam mollitia voluptatem eos minus consequatur. Quo nihil quia eos repudiandae voluptate cum quidem quisquam et omnis consequatur?</p>
+      </section>
+      {/* <h2>
         <Link href="/">Take me back home</Link>
-      </h2>
+      </h2> */}
     </Layout>
   );
 }
