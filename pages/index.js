745f6ef4c11f1fcd206002b6048095dd6020fd87
fullstackaubrey <aubreywebdev@proton.me>
2023-01-31 16:13:15 +1300

    static and server-side rendering pages added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../components/date';
+import Head from 'next/head';
 import Layout, { name, siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm {name}! I'm a Software Engineer and Yoga Instructor.</p>
         <p>
@@ -19,6 +31,22 @@ export default function Home() {
           <Link href="/posts/first-post">Take me to a new page!</ Link>
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   );
 }
